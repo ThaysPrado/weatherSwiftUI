68aba7e14c42e77d4a393885353b77d1d9541183
ThaysPrado <thaysprado96@gmail.com>
2020-01-20 13:39:56 -0300

    init project

---
+++ WeatherSwiftUI/WeatherSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherSwiftUI
+//
+//  Created by Thays Prado on 20/01/20.
+//  Copyright © 2020 ThaysPrado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
